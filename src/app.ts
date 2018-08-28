c4333f75ac09f188c4a110929369fad560823734
Tank <fuch@fullsample.com>
2018-08-28 15:50:18 +0800

    [fuchun] #Tech little spike

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,20 @@ const app:Koa = new Koa()
 
 const timeout:number = 86400
 
+const log = async (ctx:Koa.Context, next:Function) => {
+	const start:number = Date.now()
+	await next()
+	const end:number = Date.now();
+	const diff = end - start
+	console.log(`cost is: ${diff}`)
+}
+
+app.use(log)
+
 app.use(async function(ctx: Koa.Context) {
-	ctx.body = 'hello, ts koa'
+	ctx.body = `${app.env}`
 })
 
-app.listen(3000)
+
+const server = app.listen(3000)
+server.timeout = timeout
